079a1dc490e4c52e965c7acdeea270e5cb5ad5bf
THXBurke <rachelwheels@gmail.com>
2015-11-08 12:01:47 -0800

    add question five to guessing game

--- guessgame.js
+++ guessgame.js
@@ -1,30 +1,45 @@
 
-var userGuess = prompt("Would you like to play a true or false game?");
-if (userGuess === "yes") {
+var userGuess = prompt("Would you like to play a guessing game?");
+if (userGuess === "yes" || userGuess === "Yes" || userGuess === "y" || userGuess === "Y") {
 	alert("I'll try to stump you.");
 } else {
 	alert("I don't like games either but you have to play because it's my assignment.");
 };
 
-var firstQuestion = prompt("Do I have three kids?");
-if (userGuess === "true") {
-	alert("That's true! I have three kids!");
+var firstQuestion = prompt("True or False. I have three kids?");
+if (userGuess === "true" || userGuess === "True" || userGuess === "T") {
+	alert("That's true! I have three kids ages 9, 7 and 4.");
 } else {
-	alert("It's true! I have three adorable kids. Guess again.");
+	alert("It's true! Now you're surely rivited. Guess again.");
 };
 
-var secondQuestion = prompt("JavaScript come super easily to me? True or false.");
-if (userGuess === "true") {
+var secondQuestion = prompt("True or False. JavaScript come super easily to me?");
+if (userGuess === "true" || userGuess === "True" || userGuess === "T") {
 	alert("Ha! Thanks but false.");
 } else {
 	alert("True that it does not come easily but I am determined to learn!");
 };
 
-var thirdQuestion = prompt("I love long distance running.");
-if (userGuess === "true") {
+var thirdQuestion = prompt("True or False. I love long distance running.");
+if (userGuess === "true" || userGuess === "True" || userGuess === "T") {
 	alert("I used to hate it but now I actually really do love it! Gives me time to think.");
 } else {
 	alert("I used to hate it but now I actually really do love it! Gives me time to think.");
+};
+
+var age = 35;
+var fourthQuestion = prompt("Can you guess my age?");
+if (parseInt(fourthQuestion) === age ) {
+  alert ("You guessed it! I love getting older. The older you get the more you learn.");
+} else {
+  alert("Nope! I'm " + age + "!");
+         };
+
+var fifthQuestion = prompt("Do you think raising children, learning JavaScript and long distance running have anything in common?");
+if (userGuess === "Yes" || userGuess === "yes" || userGuess === "Y" || userGuess === "True" || userGuess === "true") {
+	alert("Endurance, patience, committment, hard work...you get the idea.")
+} else {
+	alert("Use your imagination!");
 };
 
 prompt("Thanks for playing my true or false game!");
