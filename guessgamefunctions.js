a5cb6cb2bd619772e81cc595e635f9a57f79d7c7
THXBurke <rachelwheels@gmail.com>
2015-11-17 19:06:22 -0800

    add guess game with the array function

--- guessgamefunctions.js
+++ guessgamefunctions.js
@@ -7,7 +7,7 @@ var array = [
 ["True or False. I love long distance running.", "I used to hate it but now I actually really do love it! Gives me time to think.", "I used to hate it but now I actually really do love it! Gives me time to think."],
 ["Do you think being 35 is pretty cool?","You guessed it! I love getting older. The older you get the more you learn.", "You guessed it! I love getting older. The older you get the more you learn."],
 ["Do you think raising children, learning JavaScript and long distance running have anything in common?","Endurance, patience, committment, hard work...you get the idea.", "Seriously? Use your imagination!" ],
-["Last but not least. Can you believe I've been married 13 years?", "You are a good guesser! And marriage fits in with our theme of things that take a lot of hard work but well worth it.", "You are a good guesser! And marriage fits in with our theme of things that take a lot of hard work but well worth it."]
+["Last but not least. Can you believe I've been married 13 years?", "You are a good guesser! And marriage fits in with our theme of things that take a lot of hard work but well worth it.", "You are a good guesser! And marriage fits in with the theme of things that take a lot of hard work but well worth it."]
 ];
 
 
@@ -16,7 +16,7 @@ function userGuess() {
 			var userGuess = prompt(array[i][0]);
 
 			if (userGuess === "true" || userGuess === "True" || userGuess === "T") {
-				alert(array[i][1]);
+				alert(array[i][1] <img src=>);
 			} else {
 			alert(array[i][2]);
 		}
@@ -24,3 +24,32 @@ function userGuess() {
 }
 
 userGuess(); 
+
+/////////
+var firstArray = document.getElementById("I'll try to stump you.");
+var secondArray = document.getElementById("That's true! I have three kids ages 9, 7 and 4.");
+var thirdArray = document.getElementById("True that it does not come easily but I am determined to learn!");
+var fourthArray = document.getElementById("I used to hate it but now I actually really do love it! Gives me time to think.");
+var fifthArray = document.getElementById("You guessed it! I love getting older. The older you get the more you learn.");
+var sixthArray = document.getElementById("Endurance, patience, committment, hard work...you get the idea.");
+var seventhArray = document.getElementById("You are a good guesser! And marriage fits in with the theme of things that take a lot of hard work but well worth it.");
+
+function userGuess() {
+	for (var i = 0; i < array.length; i++){
+			var userGuess = prompt(array[i][0]);
+
+			if (userGuess === "true" || userGuess === "True" || userGuess === "T") {
+				alert(array[i][1] <img src=>);
+			} else {
+			alert(array[i][2]);
+		}
+	}
+}
+
+userGuess(); 
+
+
+
+
+
+
