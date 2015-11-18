a961421d80e6404d69907f095d26b2bbf254798d
THXBurke <rachelwheels@gmail.com>
2015-11-18 10:14:21 -0800

    update guessgamefunctions.html,.js

--- guessgamefunctions.js
+++ guessgamefunctions.js
@@ -1,7 +1,7 @@
 
 
 var array = [ 
-["Would you like to play a guessing game?", "I'll try to stump you.", "I don't like games either but you have to play because it's my assignment." ], 
+["Would you like to play a guessing game?", "Great! I'll try to stump you.", "I don't like games either but you have to play because it's my assignment." ], 
 ["True or False. I have three kids?", "That's true! I have three kids ages 9, 7 and 4.", "It's true! Now you're surely rivited. Guess again."],
 ["True or False. JavaScript comes super easily to me?", "Ha! Thanks but false.","True that it does not come easily but I am determined to learn!"],
 ["True or False. I love long distance running.", "I used to hate it but now I actually really do love it! Gives me time to think.", "I used to hate it but now I actually really do love it! Gives me time to think."],
@@ -16,7 +16,8 @@ function userGuess() {
 			var userGuess = prompt(array[i][0]);
 
 			if (userGuess === "true" || userGuess === "True" || userGuess === "T") {
-				alert(array[i][1] <img src=>);
+				alert(array[i][1];
+					document.getElementById("myImg").src = "hackanm.gif";
 			} else {
 			alert(array[i][2]);
 		}
@@ -26,7 +27,7 @@ function userGuess() {
 userGuess(); 
 
 /////////
-var firstArray = document.getElementById("I'll try to stump you.");
+var firstArray = document.getElementById("Great! I'll try to stump you.");
 var secondArray = document.getElementById("That's true! I have three kids ages 9, 7 and 4.");
 var thirdArray = document.getElementById("True that it does not come easily but I am determined to learn!");
 var fourthArray = document.getElementById("I used to hate it but now I actually really do love it! Gives me time to think.");
@@ -34,10 +35,19 @@ var fifthArray = document.getElementById("You guessed it! I love getting older. 
 var sixthArray = document.getElementById("Endurance, patience, committment, hard work...you get the idea.");
 var seventhArray = document.getElementById("You are a good guesser! And marriage fits in with the theme of things that take a lot of hard work but well worth it.");
 
+firstArray.innerHTML = <img src ="image url" height="200px" width="200px"/>
+secondArray.innterHTML = <img src ="" height="200" width="200px"/>
+thirdArray.innterHTML = <img src ="" height="200" width="200px"/>
+forthArray.innterHTML = <img src ="" height="200" width="200px"/>
+fifthArray.innterHTML = <img src ="" height="200" width="200px"/>
+sixthArray.innterHTML = <img src ="" height="200" width="200px"/>
+seventhArray.innterHTML = <img src ="" height="200" width="200px"/>
+
+
+/////
 function userGuess() {
 	for (var i = 0; i < array.length; i++){
 			var userGuess = prompt(array[i][0]);
-
 			if (userGuess === "true" || userGuess === "True" || userGuess === "T") {
 				alert(array[i][1] <img src=>);
 			} else {
@@ -45,10 +55,7 @@ function userGuess() {
 		}
 	}
 }
-
 userGuess(); 
-
-
 
 
 
